69a0a19353dfb7aa6e5b2d2e6f5fda5ffb32bee8
awanish10singh <awanishsudoku@gmail.com>
2024-01-10 00:27:27 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3cb13933-8e06-4b30-a928-db5cd36778fa';
+export const MODAL_CLOSE_SEC = 2.5;
